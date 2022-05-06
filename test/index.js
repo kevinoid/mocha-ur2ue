f4ce9664ea11fdff63dd79f2fe85c2c568aab090
Kevin Locke <kevin@kevinlocke.name>
2022-05-06 08:10:43 -0600

    use promisify(child_process.execFile)
    Rather than providing our own Promise wrapper for execFile, use the
    custom promisify function provided in Node.js 12.6 and later.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test/index.js
+++ test/index.js
@@ -8,35 +8,26 @@
 const assert = require('assert');
 const { execFile } = require('child_process');
 const path = require('path');
+const { promisify } = require('util');
 
 const hookPath = path.join(__dirname, '..', 'index.js');
 const mochaPath = require.resolve('mocha/bin/mocha');
 const passTest = path.join(__dirname, 'fixtures', 'pass.js');
 const unhandledTest = path.join(__dirname, 'fixtures', 'unhandled.js');
 
+const execFileP = promisify(execFile);
+
 function runMocha(...args) {
-  // TODO [engine:node@>=12.6]: Use util.promisify(child_process.execFile)
-  // https://github.com/nodejs/node/pull/28325 in nodejs/node@dd5e07f9b4
-  return new Promise((resolve, reject) => {
-    // Note: Can't exec "mocha" on Windows without "shell: true", which causes
-    // perf and quoting issues, due to execFile not using %PATHEXT% which would
-    // include .cmd in search path, necessary to resolve mocha to mocha.cmd.
-    // See https://github.com/nodejs/node/issues/6671
-    const proc = execFile(
-      process.execPath,
-      [mochaPath, '-R', 'json', ...args],
-      (err, stdout, stderr) => {
-        if (err) {
-          err.stdout = stdout;
-          err.stderr = stderr;
-          reject(err);
-        } else {
-          resolve({ stdout, stderr });
-        }
-      },
-    );
-    proc.stdin.end();
-  });
+  // Note: Can't exec "mocha" on Windows without "shell: true", which causes
+  // perf and quoting issues, due to execFile not using %PATHEXT% which would
+  // include .cmd in search path, necessary to resolve mocha to mocha.cmd.
+  // See https://github.com/nodejs/node/issues/6671
+  const promise = execFileP(
+    process.execPath,
+    [mochaPath, '-R', 'json', ...args],
+  );
+  promise.child.stdin.end();
+  return promise;
 }
 
 function badPass(result) {
